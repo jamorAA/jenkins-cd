834b4cec151591b99576935cf05b770344c4dae9
ainur <4younghope4@gmail.com>
2026-03-12 13:32:33 +0300

    test CD part

--- script.groovy
+++ script.groovy
@@ -1,18 +1,22 @@
 def buildJar() {
-    echo "building the application..."
-    sh "mvn clean package"
+//    echo "building the application..."
+//    sh "mvn clean package"
 }
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker build -t jamoraa/demo-java-maven-app:1.1.0 ."
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push jamoraa/demo-java-maven-app:1.1.0"
-    }
+//    echo "building the docker image..."
+//    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+//        sh "docker build -t jamoraa/demo-java-maven-app:1.1.0 ."
+//        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+//        sh "docker push jamoraa/demo-java-maven-app:1.1.0"
+//    }
 }
 
 def deployApp() {
     echo "deploying the application..."
+    sshagent(['ssh-key']) {
+        def dockerCmd = "docker run -d -p 8080:8080 --name java-maven-app jamoraa/demo-java-maven-app:1.1.0"
+        sh "ssh -o StrictHostKeyChecking=no morant@158.160.226.219 $dockerCmd"
+    }
 }
 return this
